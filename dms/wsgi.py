b8ef6639399ed1cbc7f325dc2eecfa071e66eb7c
waruingugi <waruingugientp@gmail.com>
2024-05-15 14:33:43 +0300

    Fix wsgi to serve static files

--- dms/wsgi.py
+++ dms/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
+import os  # noqa
 
-from django.core.wsgi import get_wsgi_application
+from dj_static import Cling  # noqa
+from django.core.wsgi import get_wsgi_application  # noqa
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dms.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
